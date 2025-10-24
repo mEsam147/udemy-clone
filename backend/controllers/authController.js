d436b7b34f9aa0b9530a57724cec2de05b8a57ef
Mohamed <moesam1456@gmail.com>
2025-10-24 23:28:40 +0300

    secure is true now

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -341,7 +341,7 @@ exports.googleAuth = (req, res) => {
   // Set HTTP-only cookie
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: '/',
