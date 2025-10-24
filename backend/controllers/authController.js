dcfbbb4d11e353e72245ced10b596d39f8366f12
Mohamed <moesam1456@gmail.com>
2025-10-24 22:35:10 +0300

    fix google Auth in production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -342,7 +342,7 @@ exports.googleAuth = (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: '/',
   })
@@ -358,7 +358,7 @@ exports.githubAuth = (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: '/',
   })
